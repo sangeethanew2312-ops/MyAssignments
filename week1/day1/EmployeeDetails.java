0ef3ffcde60b47b46c3eb9b9961aba3def4b871a
sangeethanew2312-ops <sangeethanew2312@gmail.com>
2025-12-27 11:40:30 +0530

    Add files via upload
    Week1.Day1 - Assignments

---
+++ week1/day1/EmployeeDetails.java
@@ -0,0 +1,28 @@
+package week1.day1;
+
+public class EmployeeDetails {
+
+	public static void main(String[] args) {
+		String name="John Doe";
+	    byte age = 30;
+		Double salary = 55000.75;
+		char gender = 'M';
+		boolean acceptTerms=true;
+		float rating = 4.5f;
+		long mobileNumber=9876543210L;
+		int pinCode= 56001;
+		byte luckyNumber = 7;
+		
+		System.out.println("Employee Name: "+name);
+		System.out.println("Employee Age: "+age);
+		System.out.println("Employee Salary: "+salary);
+		System.out.println("Employee Gender: "+gender);
+		System.out.println("Does Employee accept terms: "+acceptTerms);
+		System.out.println("Employee rating: "+rating);
+		System.out.println("Employee Mobile Number: "+mobileNumber);
+		System.out.println("Employee Pincode: "+pinCode);
+		System.out.println("Employee luckyNumber: " +luckyNumber);
+
+	}
+
+}
